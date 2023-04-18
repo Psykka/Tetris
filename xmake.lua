9186f6f97e65d7b33dfa614e95733f214f1b929f
Psykka <36780964+Psykka@users.noreply.github.com>
2023-04-17 21:38:52 -0300

    build: change xbuild to meson

--- xmake.lua
+++
@@ -1,9 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-add_requires("libsdl 2.26.*")
-
-target("tetris")
-    set_kind("binary")
-    add_headerfiles("src/*.h")
-    add_files("src/*.c")
-    add_packages("libsdl")
